8fb89f9feaf22b0578af5bf798e27a776827228a
Mitchel Nijdam <mitchel.nijdam@teamrockstars.nl>
2024-12-01 11:31:21 +0100

    Initial commit

---
+++ src/main/kotlin/day01/Day01_part1.kt
@@ -0,0 +1,26 @@
+package day01
+
+import printOutput
+import printTestOutput
+import readInput
+import readTestInput
+
+fun main() {
+    val day = 1
+    val part = 1
+    println("\n\n")
+
+    fun solvePuzzle(input: List<String>): Int {
+        return input.size
+    }
+
+    val testInput = readTestInput(day, part)
+    val testSolution = solvePuzzle(testInput)
+
+    printTestOutput(testInput, testSolution)
+
+    val input = readInput(day, part)
+    val solution = solvePuzzle(input)
+
+    printOutput(solution)
+}
