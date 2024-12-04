bcb30fe54c4b98220239d8d0679d3074d7233773
Mitchel Nijdam <mitchel.nijdam@teamrockstars.nl>
2024-12-04 08:32:28 +0100

    day 3 done

--- src/main/kotlin/day03/Day03_part1.kt
+++ src/main/kotlin/day03/Day03_part1.kt
@@ -10,8 +10,16 @@ fun main() {
     val part = 1
     println("\nExecuting part $part of day $day\n\n")
 
-    fun solvePuzzle(input: List<String>): Int {
-        return input.size
+    fun parseInput(input: List<String>): List<Pair<Int, Int>> {
+        val regex = Regex("mul\\((\\d{1,3}),(\\d{1,3})\\)")
+        return input.flatMap { line ->
+            regex.findAll(line).map { it.groupValues[1].toInt() to it.groupValues[2].toInt() }
+        }
+    }
+
+    fun solvePuzzle(input: List<String>): Long {
+        val multiplyPairs = parseInput(input)
+        return multiplyPairs.sumOf { (l, r) -> l.toLong() * r.toLong() }
     }
 
     val testInput = readTestInput(day)
--- src/main/kotlin/day03/Day03_part2.kt
+++ src/main/kotlin/day03/Day03_part2.kt
@@ -2,16 +2,40 @@ package day03
 
 import printOutput
 import printTestOutput
+import println
 import readInput
 import readTestInput
 
 fun main() {
     val day = 3
-    val part = 2
+    val part = 1
     println("\nExecuting part $part of day $day\n\n")
 
-    fun solvePuzzle(input: List<String>): Int {
-        return input.size
+    fun parseInput(input: List<String>): List<Pair<Int, Int>> {
+        val regex = Regex("mul\\((\\d{1,3}),(\\d{1,3})\\)|don't\\(\\)|do\\(\\)")
+        var enabled = true
+
+        return input.flatMap { line ->
+            regex.findAll(line).mapNotNull { match ->
+                if (match.value == "don't()") {
+                    enabled = false
+                    return@mapNotNull null
+                }
+                if (match.value == "do()") {
+                    enabled = true
+                    return@mapNotNull null
+                }
+                if (!enabled) return@mapNotNull null
+
+                match.groupValues.println()
+                return@mapNotNull match.groupValues[1].toInt() to match.groupValues[2].toInt()
+            }
+        }
+    }
+
+    fun solvePuzzle(input: List<String>): Long {
+        val multiplyPairs = parseInput(input)
+        return multiplyPairs.sumOf { (l, r) -> l.toLong() * r.toLong() }
     }
 
     val testInput = readTestInput(day)
