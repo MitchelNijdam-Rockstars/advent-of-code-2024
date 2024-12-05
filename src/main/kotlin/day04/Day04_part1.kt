d623a3e7791929ca035985959fe28e8ef3b7e6dc
Mitchel Nijdam <mitchel.nijdam@teamrockstars.nl>
2024-12-05 06:22:52 +0100

    day 4 done

--- src/main/kotlin/day04/Day04_part1.kt
+++ src/main/kotlin/day04/Day04_part1.kt
@@ -1,17 +1,57 @@
 package day04
 
+import java.nio.file.Files.move
 import printOutput
 import printTestOutput
 import readInput
 import readTestInput
+
+private const val X_MAS = "XMAS"
 
 fun main() {
     val day = 4
     val part = 1
     println("\nExecuting part $part of day $day\n\n")
 
+    fun isOutsideBox(value: Int, max: Int): Boolean {
+        return value < 0 || value > max
+    }
+
+    fun countXmas(startCoordinates: StartCoordinates, input: List<String>): Int {
+        val width = input.first().length
+        val height = input.size
+
+        val movementToValid = Movement.entries.map { movement ->
+            var currentX = startCoordinates.x
+            var currentY = startCoordinates.y
+
+            for (char in X_MAS) {
+                if (isOutsideBox(currentX, width - 1) || isOutsideBox(currentY, height - 1))
+                    return@map movement to false
+
+                val inputChar = input[currentY][currentX]
+                val isEqual = inputChar == char
+                if (!isEqual) return@map movement to false
+                currentX = currentX + movement.deltaX
+                currentY = currentY + movement.deltaY
+
+            }
+            movement to true
+        }
+
+        val count = movementToValid.toMap().values.count { it == true }
+        println("for $startCoordinates, count = $count, $movementToValid")
+        return count
+    }
+
     fun solvePuzzle(input: List<String>): Int {
-        return input.size
+        val startCoordinates = input.mapIndexed { y, line ->
+            line.mapIndexed { x, ch -> if (ch == 'X') StartCoordinates(x, y) else null }.filterNotNull()
+        }.flatten()
+
+        return startCoordinates.sumOf { coordinates ->
+            countXmas(coordinates, input)
+        }
     }
 
     val testInput = readTestInput(day)
@@ -23,4 +63,20 @@ fun main() {
     val solution = solvePuzzle(input)
 
     printOutput(solution)
+}
+
+data class StartCoordinates(
+    val x: Int,
+    val y: Int
+)
+
+enum class Movement(val deltaX: Int, val deltaY: Int) {
+    HOR_RIGHT(1, 0),
+    HOR_LEFT(-1, 0),
+    DIA_TL(-1, -1),
+    DIA_TR(1, -1),
+    DIA_BL(-1, 1),
+    DIA_BR(1, 1),
+    VER_TOP(0, -1),
+    VER_BOTTOM(0, 1);
 }
--- src/main/kotlin/day04/Day04_part2.kt
+++ src/main/kotlin/day04/Day04_part2.kt
@@ -5,13 +5,58 @@ import printTestOutput
 import readInput
 import readTestInput
 
+
 fun main() {
     val day = 4
-    val part = 2
+    val part = 1
     println("\nExecuting part $part of day $day\n\n")
 
+    fun isOutsideBox(value: Int, max: Int): Boolean {
+        return value < 0 || value > max
+    }
+
+    fun countXmas(startCoordinates: StartCoordinates, input: List<String>): Int {
+        val width = input.first().length
+        val height = input.size
+
+        val ms: List<Move> = Move.entries.mapNotNull { movement ->
+            val x = startCoordinates.x + movement.deltaX
+            val y = startCoordinates.y + movement.deltaY
+            if (isOutsideBox(x, width - 1) || isOutsideBox(y, height - 1)) return@mapNotNull null
+            if (input[y][x] == 'M') {
+                return@mapNotNull movement
+            }
+            null
+        }
+        if (ms.count() != 2) return 0
+
+        for (movement in ms) {
+            val opposite = movement.getOpposite()
+            val x = startCoordinates.x + opposite.deltaX
+            val y = startCoordinates.y + opposite.deltaY
+            if (isOutsideBox(x, width - 1) || isOutsideBox(y, height - 1)) return 0
+            if (input[y][x] == 'M') return 0
+        }
+
+        val ss = Move.entries.count { movement ->
+            val x = startCoordinates.x + movement.deltaX
+            val y = startCoordinates.y + movement.deltaY
+            if (isOutsideBox(x, width - 1) || isOutsideBox(y, height - 1)) return@count false
+            input[y][x] == 'S'
+        }
+        if (ss != 2) return 0
+
+        return 1
+    }
+
     fun solvePuzzle(input: List<String>): Int {
-        return input.size
+        val startCoordinates = input.mapIndexed { y, line ->
+            line.mapIndexed { x, ch -> if (ch == 'A') StartCoordinates(x, y) else null }.filterNotNull()
+        }.flatten()
+
+        return startCoordinates.sumOf { coordinates ->
+            countXmas(coordinates, input)
+        }
     }
 
     val testInput = readTestInput(day)
@@ -23,4 +68,20 @@ fun main() {
     val solution = solvePuzzle(input)
 
     printOutput(solution)
+}
+
+private enum class Move(val deltaX: Int, val deltaY: Int) {
+    TL(-1, -1),
+    TR(1, -1),
+    BL(-1, 1),
+    BR(1, 1);
+
+    fun getOpposite(): Move {
+        return when (this) {
+            TL -> BR
+            TR -> BL
+            BL -> TR
+            BR -> TL
+        }
+    }
 }
